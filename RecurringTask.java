1306989331bac5e19ae4df393dd5074249b7b5af
sarabahri <sarabahri15@gmail.com>
2023-10-27 16:48:22 -0400

    RecurringTask added

--- RecurringTask.java
+++ RecurringTask.java
@@ -1,3 +1,13 @@
 public class RecurringTask extends Task {
-    
+    public RecurringTask(String title, String description, String date, String status,
+                         String priority) {
+        super(title, description, date, status, priority);
+    }
+
+    public void inProgress() {
+        String status = "pending";
+    }
+    public void finishedTask() {
+        String status = "complete";
+    }
 }
